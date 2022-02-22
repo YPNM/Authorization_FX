62d6b1ec7332ce0d620b1151f8cadb3ff6768bf3
Young <games.art02@mail.ru>
2022-02-23 05:14:36 +0600

    Запуск графической оболочки

--- src/main/java/com/example/authorization_fx/Controller.java
+++ src/main/java/com/example/authorization_fx/Controller.java
@@ -4,7 +4,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -19,13 +18,10 @@ public class Controller {
     private URL location;
 
     @FXML
-    private MenuItem about;
-
-    @FXML
-    private MenuItem add_tovar;
-
-    @FXML
     private TextField bar_code;
+
+    @FXML
+    private Button buy;
 
     @FXML
     private TableColumn<?, ?> cost;
@@ -37,33 +33,28 @@ public class Controller {
     private Button deny;
 
     @FXML
-    private Text final_price;
-
-    @FXML
-    private TableView<?> main_table;
-
-    @FXML
-    private Button pay;
+    private Text main_price;
 
     @FXML
     private TableColumn<?, ?> price;
+
+    @FXML
+    private TableView<?> table;
 
     @FXML
     private TableColumn<?, ?> tovar;
 
     @FXML
     void initialize() {
-        assert about != null : "fx:id=\"about\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert add_tovar != null : "fx:id=\"add_tovar\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert bar_code != null : "fx:id=\"bar_code\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert cost != null : "fx:id=\"cost\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert count != null : "fx:id=\"count\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert deny != null : "fx:id=\"deny\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert final_price != null : "fx:id=\"final_price\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert main_table != null : "fx:id=\"main_table\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert pay != null : "fx:id=\"pay\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert price != null : "fx:id=\"price\" was not injected: check your FXML file 'fxml.fxml'.";
-        assert tovar != null : "fx:id=\"tovar\" was not injected: check your FXML file 'fxml.fxml'.";
+        assert bar_code != null : "fx:id=\"bar_code\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert buy != null : "fx:id=\"buy\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert cost != null : "fx:id=\"cost\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert count != null : "fx:id=\"count\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert deny != null : "fx:id=\"deny\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert main_price != null : "fx:id=\"main_price\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert price != null : "fx:id=\"price\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert table != null : "fx:id=\"table\" was not injected: check your FXML file 'main-frame.fxml'.";
+        assert tovar != null : "fx:id=\"tovar\" was not injected: check your FXML file 'main-frame.fxml'.";
 
     }
 
---
+++ src/main/java/com/example/authorization_fx/MainApplication.java
@@ -0,0 +1,26 @@
+package com.example.authorization_fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class MainApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Paths.get("D:\\Универ\\IT\\1 курс\\2 трим\\Java\\Authorization_FX\\src\\main\\resources\\com\\example\\authorization_fx\\main-frame.fxml").toUri().toURL());
+        Scene scene = new Scene(root, 900, 600);
+        stage.setTitle("Автоматиз");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
