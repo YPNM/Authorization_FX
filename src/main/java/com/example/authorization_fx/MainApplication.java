1ad78e59e924c3290746233580a99708bb0c8e7d
Young <games.art02@mail.ru>
2022-02-24 22:36:12 +0600

    Добавление изменения общей цены

--- src/main/java/com/example/authorization_fx/MainApplication.java
+++ src/main/java/com/example/authorization_fx/MainApplication.java
@@ -9,11 +9,13 @@ import javafx.stage.Stage;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(Paths.get("D:\\Универ\\IT\\1 курс\\2 трим\\Java\\Authorization_FX\\src\\main\\resources\\com\\example\\authorization_fx\\main-frame.fxml").toUri().toURL());
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-frame.fxml")));
+        //Parent root = FXMLLoader.load(Paths.get("D:\\Универ\\IT\\1 курс\\2 трим\\Java\\Authorization_FX\\src\\main\\resources\\com\\example\\authorization_fx\\main-frame.fxml").toUri().toURL());
         Scene scene = new Scene(root, 900, 600);
         stage.setTitle("Автоматиз");
         stage.setScene(scene);
